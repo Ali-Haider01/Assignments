60cfd81d7677acc1246ef8f9b620959db95ad2d0
Ali-Haider01 <msalihaider21@gmail.com>
2023-09-15 22:48:57 -0700

    Assignment

--- Test-6/test06.js
+++ Test-6/test06.js
@@ -1,4 +1,4 @@
 "use strict";
 // 6). Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8.log
 //  Be sure to enclose your operations in print statements to see the results.
-console.log("Addition : ", 5 + 3, "\tSubtraction : ", 15 - 9, "\tMultiplication : ", 2 * 4, "\tDivision : ", 16 / 2);
+console.log("Addition : ", 5 + 3, "Subtraction : ", 15 - 9, "\tMultiplication : ", 2 * 4, "\tDivision : ", 16 / 2);
